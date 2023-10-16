3dd36f07b820dcde4ca27f5a71fc39b83c028f7a
jacam666 <ja6cam@gmail.com>
2023-10-16 19:55:21 +0100

    added new images for shoulder page that are matching

--- src/pages/Shoulders.js
+++ src/pages/Shoulders.js
@@ -8,24 +8,24 @@ function Shoulders() {
         <div className='shoulders-container'>
             <div className='shoulder-page-header'>Shoulders</div>
             <div className='card-container row justify-content-evenly' >
-                <div className='col-lg-2 col-md-4 col-sm-6'>
+                {/* <div className='col-lg-2 col-md-4 col-sm-6'>
                     <div className="d-flex flex-column h-100">
-                        <Card className="flex-fill mt-4" style={{ opacity: 0.9 }}>
+                        <Card className="flex-fill mb-4" style={{ opacity: 0.9 }}>
                             <Card.Img className='shoulder-images' variant="top" src="/images/Shoulders/shoulders face pull.png" />
                             <Card.Body>
                                 <Card.Title>Face Pull</Card.Title>
                                 <Card.Text>
 
                                 </Card.Text>
-                                {/* <Button variant="primary">Go somewhere</Button> */}
+                                
                             </Card.Body>
                         </Card>
                     </div>
-                </div>
+                </div> */}
                 <div className='col-lg-2 col-md-4 col-sm-6'>
                     <div className="d-flex flex-column h-100">
-                        <Card className="flex-fill mt-4" style={{ opacity: 0.9 }}>
-                            <Card.Img className='shoulder-images' variant="top" src="/images/Shoulders/shoulders reverse fly.png" />
+                        <Card className="flex-fill mb-4" style={{ opacity: 0.9 }}>
+                            <Card.Img className='shoulder-images' variant="top" src="/images/Shoulders/shoulders reverse fly2.png" />
                             <Card.Body>
                                 <Card.Title>Reverse Fly</Card.Title>
                                 <Card.Text>
@@ -38,10 +38,10 @@ function Shoulders() {
                 </div>
                 <div className='col-lg-2 col-md-4 col-sm-6'>
                     <div className="d-flex flex-column h-100">
-                        <Card className="flex-fill mt-4" style={{ opacity: 0.9 }}>
-                            <Card.Img className='shoulder-images' variant="top" src="/images/Shoulders/shoulders kneeling lift.png" />
+                        <Card className="flex-fill mb-4" style={{ opacity: 0.9 }}>
+                            <Card.Img className='shoulder-images' variant="top" src="/images/Shoulders/shoulders lying front shoulder raise.png" />
                             <Card.Body>
-                                <Card.Title>Kneeling Lift</Card.Title>
+                                <Card.Title>Lying Front Raise</Card.Title>
                                 <Card.Text>
 
                                 </Card.Text>
@@ -52,8 +52,8 @@ function Shoulders() {
                 </div>
                 <div className='col-lg-2 col-md-4 col-sm-6'>
                     <div className="d-flex flex-column h-100">
-                        <Card className="flex-fill mt-4" style={{ opacity: 0.9 }}>
-                            <Card.Img className='shoulder-images' variant="top" src="/images/Shoulders/shoulders high row.png" />
+                        <Card className="flex-fill mb-4" style={{ opacity: 0.9 }}>
+                            <Card.Img className='shoulder-images' variant="top" src="/images/Shoulders/shoulders high row2.png" />
                             <Card.Body>
                                 <Card.Title>High Row</Card.Title>
                                 <Card.Text>
@@ -66,8 +66,8 @@ function Shoulders() {
                 </div>
                 <div className='col-lg-2 col-md-4 col-sm-6'>
                     <div className="d-flex flex-column h-100">
-                        <Card className="flex-fill mt-4" style={{ opacity: 0.9 }}>
-                            <Card.Img className='shoulder-images' variant="top" src="/images/Shoulders/shoulders front raise.png" />
+                        <Card className="flex-fill mb-4" style={{ opacity: 0.9 }}>
+                            <Card.Img className='shoulder-images' variant="top" src="/images/Shoulders/shoulders front raises.png" />
                             <Card.Body>
                                 <Card.Title>Front Raise</Card.Title>
                                 <Card.Text>
@@ -80,10 +80,38 @@ function Shoulders() {
                 </div>
                 <div className='col-lg-2 col-md-4 col-sm-6'>
                     <div className="d-flex flex-column h-100">
-                        <Card className="flex-fill mt-4" style={{ opacity: 0.9 }}>
-                            <Card.Img className='shoulder-images' variant="top" src="/images/Shoulders/shoulders lateral raises2.png" />
+                        <Card className="flex-fill mb-4" style={{ opacity: 0.9 }}>
+                            <Card.Img className='shoulder-images' variant="top" src="/images/Shoulders/shoulders lateral raises3.png" />
                             <Card.Body>
                                 <Card.Title>Lateral Raises</Card.Title>
+                                <Card.Text>
+
+                                </Card.Text>
+                                {/* <Button variant="primary">Go somewhere</Button> */}
+                            </Card.Body>
+                        </Card>
+                    </div>
+                </div>
+                <div className='col-lg-2 col-md-4 col-sm-6'>
+                    <div className="d-flex flex-column h-100">
+                        <Card className="flex-fill mb-4" style={{ opacity: 0.9 }}>
+                            <Card.Img className='shoulder-images' variant="top" src="/images/Shoulders/shoulders lying lateral raise.png" />
+                            <Card.Body>
+                                <Card.Title>Lying Lateral Raise</Card.Title>
+                                <Card.Text>
+
+                                </Card.Text>
+                                {/* <Button variant="primary">Go somewhere</Button> */}
+                            </Card.Body>
+                        </Card>
+                    </div>
+                </div>
+                <div className='col-lg-2 col-md-4 col-sm-6'>
+                    <div className="d-flex flex-column h-100">
+                        <Card className="flex-fill mb-4" style={{ opacity: 0.9 }}>
+                            <Card.Img className='shoulder-images' variant="top" src="/images/Shoulders/shoulders seated high row.png" />
+                            <Card.Body>
+                                <Card.Title>Seated High Row</Card.Title>
                                 <Card.Text>
 
                                 </Card.Text>
