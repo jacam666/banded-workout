99d11ef1c076acba6baa3a5a5daaaafa93484505
jacam666 <ja6cam@gmail.com>
2023-10-25 20:41:50 +0100

    added input functionality to the training page diary table

--- src/pages/TrainingDiaryPage.js
+++ src/pages/TrainingDiaryPage.js
@@ -1,74 +1,73 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { MDBTable, MDBTableHead, MDBTableBody } from 'mdb-react-ui-kit';
 import "../styles/TrainingDiary.css"
 
+
 export default function TrainingDiaryPage() {
+    const initialExerciseData = new Array(10).fill({ name: '', weight: '', sets: '', reps: '' });
+    const [exerciseData, setExerciseData] = useState(initialExerciseData);
+
+    const handleCellChange = (rowIndex, columnName, value) => {
+        const updatedData = [...exerciseData];
+        updatedData[rowIndex] = { ...updatedData[rowIndex], [columnName]: value };
+        setExerciseData(updatedData);
+    };
+
     return (
-        <div className='table-container'>
-            <MDBTable bordered>
-            <MDBTableHead>
-                <tr>
-                    <th scope='col'>Exercise</th>
-                    <th scope='col'>Weight</th>
-                    <th scope='col'>Sets</th>
-                    <th scope='col'>Reps</th>
-                </tr>
-            </MDBTableHead>
-            <MDBTableBody>
-                <tr>
-                    <th scope='row'>1</th>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                </tr>
-                <tr>
-                    <th scope='row'>2</th>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                </tr>
-                <tr>
-                    <th scope='row'>3</th>
-                    <td></td>
-                    <td></td>
-                </tr>
-                <tr>
-                    <th scope='row'>4</th>
-                    <td></td>
-                    <td></td>
-                </tr>
-                <tr>
-                    <th scope='row'>5</th>
-                    <td></td>
-                    <td></td>
-                </tr>
-                <tr>
-                    <th scope='row'>6</th>
-                    <td></td>
-                    <td></td>
-                </tr>
-                <tr>
-                    <th scope='row'>7</th>
-                    <td></td>
-                    <td></td>
-                </tr>
-                <tr>
-                    <th scope='row'>8</th>
-                    <td></td>
-                    <td></td>
-                </tr>
-                <tr>
-                    <th scope='row'>9</th>
-                    <td></td>
-                    <td></td>
-                </tr>
-                <tr>
-                    <th scope='row'>10</th>
-                    <td></td>
-                    <td></td>
-                </tr>
-            </MDBTableBody>
-        </MDBTable>
-        </div>
+        <div className="training-container">
+            {/* <div className="row"> */}
+                {/* <div className="col-lg-12 col-md-12 col-sm-12"> */}
+                    <MDBTable bordered>
+                        <MDBTableHead>
+                            <tr>
+                                <th scope='col'>Exercise</th>
+                                <th scope='col'>Weight</th>
+                                <th scope='col'>Sets</th>
+                                <th scope='col'>Reps</th>
+                            </tr>
+                        </MDBTableHead>
+                        <MDBTableBody>
+                            {exerciseData.map((exercise, rowIndex) => (
+                                <tr key={rowIndex}>
+                                    <th scope='row'>{rowIndex + 1}</th>
+                                    <td>
+                                        <input className='input-container'
+                                            type="text"
+                                            value={exercise.name}
+                                            onChange={(e) => handleCellChange(rowIndex, 'name', e.target.value)}
+                                            key={`name-${rowIndex}`}
+                                        />
+                                    </td>
+                                    <td>
+                                        <input className='input-container'
+                                            type="text"
+                                            value={exercise.weight}
+                                            onChange={(e) => handleCellChange(rowIndex, 'weight', e.target.value)}
+                                            key={`weight-${rowIndex}`}
+                                        />
+                                    </td>
+                                    <td>
+                                        <input className='input-container'
+                                            type="text"
+                                            value={exercise.sets}
+                                            onChange={(e) => handleCellChange(rowIndex, 'sets', e.target.value)}
+                                            key={`sets-${rowIndex}`}
+                                        />
+                                    </td>
+                                    <td>
+                                        <input className='input-container'
+                                            type="text"
+                                            value={exercise.reps}
+                                            onChange={(e) => handleCellChange(rowIndex, 'reps', e.target.value)}
+                                            key={`reps-${rowIndex}`}
+                                        />
+                                    </td>
+                                </tr>
+                            ))}
+                        </MDBTableBody>
+                    </MDBTable>
+                </div>
+            // </div>
+        // </div>
     );
 }
